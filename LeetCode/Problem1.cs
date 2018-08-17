8c8ef8cfd7ec9818d88bd98bf023906bfac68c8d
dmytrofisher <dmytrofisher@gmail.com>
2018-08-17 03:19:32 +0300

    Problem #1

---
+++ LeetCode/Problem1.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode
+{
+	using System.Collections.Generic;
+
+	/// <summary>
+	/// Two Sum
+	/// https://leetcode.com/problems/two-sum/description/
+	/// </summary>
+	public class Problem1
+	{
+
+		public int[] TwoSum(int[] nums, int target) {
+			var complements = new Dictionary<int, int>();
+			for (int i = 0; i < nums.Length; i++) {
+				int num = nums[i];
+				if (complements.ContainsKey(num)) {
+					return new int[] { complements[num], i };
+				} else if (!complements.ContainsKey(target - num)) {
+					complements.Add(target - num, i);
+				}
+			}
+			return new int[] { };
+		}
+
+	}
+
+}
