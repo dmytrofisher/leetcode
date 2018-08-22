5e1974f3b365d510522c3d87f539ed2d3b47cd47
Dmytro Slobodianyk <dmytrofisher@gmail.com>
2018-08-22 19:45:01 +0300

    Problem 2 Tests

---
+++ LeetCode.Tests/Problem1Tests.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Tests
+{
+	using LeetCode;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class Problem1Tests
+	{
+		[Test]
+		public void Test1() {
+			// Arrage
+			int[] nums = { 2, 7, 11, 15 };
+			const int target = 9;
+			int[] expected = { 0, 1 };
+
+			// Act
+			int[] actual = new Problem1().TwoSum(nums, target);
+
+			// Assert
+			CollectionAssert.AreEqual(expected, actual);
+		}
+
+	}
+
+}
